22f332dade0e4d5e5c4dbb345bca915a288f5c1c
David Shaw <dshaw.jobs@gmail.com>
2012-06-09 01:50:50 -0700

    minor spacing update

--- ril2bookmark.sh
+++ ril2bookmark.sh
@@ -13,7 +13,7 @@ date=$(date +%s);
 echo -n '<!DOCTYPE NETSCAPE-Bookmark-file-1>
 <!-- This is an automatically generated file.
      It will be read and overwritten.
-	  DO NOT EDIT! -->
+     DO NOT EDIT! -->
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
 <TITLE>Bookmarks</TITLE>
 <H1>Bookmarks</H1>
